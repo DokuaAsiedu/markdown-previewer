a1f40ad0fa6ea547014f5785f41218a4c11171a6
DokuaAsiedu <asiedudokua@gmail.com>
2024-04-01 17:06:26 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { marked } from "marked";
+import DOMPurify from "dompurify";
+import { sampleText } from "./utils";
+import { FaFreeCodeCamp } from "react-icons/fa";
+import { TfiFullscreen } from "react-icons/tfi";
+import PropTypes from "prop-types";
+
+export default function App() {
+	const [content, setContent] = useState(sampleText);
+	const [editorFullScreen, setEditorFullScreen] = useState(false);
+	const [previewerFullScreen, setPreviewerFullScreen] = useState(false);
+
+	const handleChange = (event) => {
+		setContent(event.target.value);
+	};
+
+	const handleEditor = () => {
+		setEditorFullScreen((prev) => !prev);
+	};
+
+	const handlePreview = () => {
+		setPreviewerFullScreen((prev) => !prev);
+	};
+
+	useEffect(() => {
+		console.log(editorFullScreen);
+	});
+
+	return (
+		<div className="flex flex-col items-center justify-start gap-4 py-12 h-full min-h-screen bg-[#87B5B5] text-black relative">
+			{!previewerFullScreen && (
+				<div
+					className={`w-3/4 md:w-2/5 flex flex-col items-stretch justify-start overflow-y-auto ${
+						editorFullScreen ? "flex-1" : ""
+					}`}
+				>
+					<TitleBar title="Editor" handleClick={handleEditor} />
+					<textarea
+						onInput={handleChange}
+						className={`w-full h-full min-h-52 p-2 bg-[#C0D7D8] resize-none rounded-b-md overflow-y-auto ${
+							editorFullScreen ? "flex-1" : ""
+						}`}
+						defaultValue={content}
+					></textarea>
+				</div>
+			)}
+			{!editorFullScreen && (
+				<div
+					className={`w-11/12 md:w-3/5 flex flex-col items-stretch justify-start overflow-y-auto ${
+						previewerFullScreen ? "flex-1" : ""
+					}`}
+				>
+					<TitleBar title="Previewer" handleClick={handlePreview} />
+					<div
+						className={`w-full h-full min-h-60 px-4 pt-2 pb-4 bg-[#C0D7D8] rounded-b-md overflow-y-auto ${
+							previewerFullScreen ? "flex-1" : ""
+						}`}
+						dangerouslySetInnerHTML={{
+							__html: DOMPurify.sanitize(marked.parse(content)),
+						}}
+					></div>
+				</div>
+			)}
+		</div>
+	);
+}
+
+function TitleBar({ title, handleClick }) {
+	return (
+		<div className="w-full h-min flex flex-row items-end justify-start gap-2 bg-[#4AA3A3] p-2 rounded-t-md font-extrabold">
+			<FaFreeCodeCamp />
+			<span className="inline-block align-bottom">{title}</span>
+			<TfiFullscreen className="ms-auto" onClick={handleClick} />
+		</div>
+	);
+}
+
+TitleBar.propTypes = {
+	title: PropTypes.string,
+	handleClick: PropTypes.func,
+};
